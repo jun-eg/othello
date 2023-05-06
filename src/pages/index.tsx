420cd6a96a2e585969d0743bdfff8fe48d7c7ddc
s1f102300144 <s1f102300144@iniad.org>
2023-05-06 11:06:02 +0900

    8方向関数修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,24 +32,28 @@ const Home = () => {
 
     //8方向を参照して対駒座標、返し駒座標リストを返す関数
     function look_around(assume_x: number, assume_y: number): [number[], number[][]] {
-      //確定返し駒リスト※y,xの順番で格納されてる
+      //確定返し駒リスト※x,yの順番で格納されてる
       let return_piece_list: number[][] = [];
 
-      //対駒座標
-      const same_piece: number[] = [];
+      //クリック座標
+      let valid_click_state: number[] = [];
 
       for (const course of directions) {
-        //臨時返し駒リスト※y,xの順番で格納されてる
+        //臨時返し駒リスト※x,yの順番で格納されてる
         const temporary_return_piece_list: number[][] = [];
 
         //1つ隣が異色駒の場合は、2つ以降の隣コマ参照
-        if (board[y + course[1]][x + course[0]] === 3 - turnColor) {
+        if (
+          board[y + course[1]] !== undefined &&
+          board[x + course[0]] !== undefined &&
+          board[y + course[1]][x + course[0]] === 3 - turnColor
+        ) {
           for (let next_squares = 2; next_squares <= 7; next_squares++) {
             const x_next_squares = course[0] * next_squares + assume_x;
             const y_next_squares = course[1] * next_squares + assume_y;
 
-            //返し駒リスト格納
-            temporary_return_piece_list.push([y_next_squares, x_next_squares]);
+            //臨時返し駒リストへ格納※pushの順番注意
+            temporary_return_piece_list.push([x_next_squares, y_next_squares]);
 
             if (
               //対駒の前に0が来たらbreak
@@ -60,13 +64,19 @@ const Home = () => {
               break;
             }
 
-            if (board[y_next_squares][x_next_squares] === turnColor) {
-              //対駒がある場合、臨時返し駒リストの座標をリスト
-              return_piece_list = temporary_return_piece_list;
+            if (
+              board[y + course[1]] !== undefined &&
+              board[x + course[0]] !== undefined &&
+              board[y_next_squares][x_next_squares] === turnColor
+            ) {
+              //異色の場合、1つ隣のマスを臨時返し駒リストへ格納
+              temporary_return_piece_list.push([assume_x + course[0], assume_y + course[1]]);
 
-              //対駒座標取得
-              same_piece.push(y_next_squares);
-              same_piece.push(x_next_squares);
+              //対駒がある場合、臨時返し駒リストの座標をリストへ
+              return_piece_list = return_piece_list.concat(temporary_return_piece_list);
+
+              //対駒が認識された場合、クリック座標を格納※x,yの順で格納
+              valid_click_state = [assume_x, assume_y];
 
               break;
             }
@@ -74,7 +84,26 @@ const Home = () => {
         }
       }
 
-      return [same_piece, return_piece_list];
+      return [valid_click_state, return_piece_list];
+    }
+
+    const [click, return_list] = look_around(x, y);
+
+    // console.log('click', click === []);
+    console.log('return_list', return_list, return_list[0], return_list[1]);
+    if (
+      (board[click[0]] !== undefined &&
+        board[click[1]] !== undefined &&
+        board[click[1]][click[0]] === 3) ||
+      board[click[1]][click[0]] === 0
+    ) {
+      newBoard[click[1]][click[0]] = turnColor;
+
+      for (const one_return_list of return_list) {
+        newBoard[one_return_list[1]][one_return_list[0]] = turnColor;
+      }
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
     }
   };
   return (
